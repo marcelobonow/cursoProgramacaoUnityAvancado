ab32775d257e392efe383ee412b1ad2736f3c1b6
Everton Vieira <everton.vieira@izyplay.com.br>
2018-05-01 11:58:26 -0300

    Aula 1

---
+++ Aula1/Assets/Scripts/Aula1/TriggerPushUpBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPushUpBehaviour : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter com " + other.name);
+        other.GetComponent<Rigidbody>().velocity = Vector3.up*10;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerEnter com " + other.name);
+        other.GetComponent<Rigidbody>().velocity = Vector3.up * 10;
+    }
+
+}
