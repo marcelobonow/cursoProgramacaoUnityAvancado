fa85094d50fec6820c5511e9d28933b415dcc473
Everton Vieira <everton.vieira@izyplay.com.br>
2018-05-05 19:04:31 -0300

    Troca de nome Projeto 1

---
+++ Projeto1/Assets/Scripts/Aula3/LERPDeltaTimeMovimentBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class LERPDeltaTimeMovimentBehaviour : MonoBehaviour {
+    public Transform targePosition;
+    public float timeToComplete;
+
+    protected Vector3 startPoistion;
+    protected float deltaTimeCounter;
+    
+	// Use this for initialization
+	void Start () {
+        deltaTimeCounter = 0;
+        startPoistion = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deltaTimeCounter += Time.deltaTime/ timeToComplete;
+        Debug.Log(deltaTimeCounter);
+        GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(startPoistion, targePosition.position, deltaTimeCounter));
+	}
+}
---
+++ Projeto1/Assets/Scripts/PinFootBall/BallKicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallKicker : MonoBehaviour {
+    public GameObject ball;
+    public float force=500;
+    public float turnSpeed = 1;
+
+    // Update is called once per frame
+    void Update () {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        if (horizontalAxis!=0)
+            transform.Rotate(0, horizontalAxis* turnSpeed, 0);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 forward = transform.forward;
+            forward.y += 0.5f;
+            ball.GetComponent<Rigidbody>().AddForce(forward * force);
+        }
+    }
+}
