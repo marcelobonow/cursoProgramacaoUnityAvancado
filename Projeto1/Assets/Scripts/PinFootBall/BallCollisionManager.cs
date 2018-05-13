36be9d507c0d492724edc9a9f40f19bdf153ec5f
Everton Vieira <everton.vieira@izyplay.com.br>
2018-05-13 18:28:01 -0300

    Ajustes na Aula 1 Código para as Aulas 2 e 3

--- Projeto1/Assets/Scripts/PinFootBall/BallCollisionManager.cs
+++ Projeto1/Assets/Scripts/PinFootBall/BallCollisionManager.cs
@@ -6,6 +6,13 @@ public class BallCollisionManager : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        //Informa o manager quando colidou com um outro objeto.
+        PinFootBallManager.instance.onGoal(other.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Informa o manager quando colidou com um outro objeto.
+        PinFootBallManager.instance.onGoal(collision.collider.gameObject);
     }
 }
---
+++ Projeto1/Assets/Scripts/PinFootBall/PinFootBallManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinFootBallManager : MonoBehaviour {
+
+    public static PinFootBallManager instance;
+    public TextMeshProUGUI scoreTeamAField, scoreTeamBField;
+    private int scoreTeamA, scoreTeamB = 0;
+
+
+    private void Awake()
+    {
+        if (instance == null) //Se a variável estática estiver nula,
+        {
+            instance = this; //Atribui o OBJETO GameManager a variável estática.
+            DontDestroyOnLoad(gameObject);//Define que o objeto não deve ser destruído nas cargas
+        }else if (instance != this) //Se a variável for diferente de this, já foi criada antes
+            Destroy(gameObject);//Destrói o objeto    
+    }
+
+    public void onGoal(GameObject target)
+    {
+        Debug.Log(target.name);
+        if (target.tag == "TeamA")
+            scoreTeamAField.text = (++scoreTeamA).ToString();
+        else if (target.tag == "TeamB")
+            scoreTeamBField.text = (++scoreTeamB).ToString();
+    }
+}
