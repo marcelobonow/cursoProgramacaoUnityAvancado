8eaa5ec77b08cddbd1af9b939d498242e655f5bf
Everton Vieira <everton.vieira@izyplay.com.br>
2018-06-20 19:57:28 -0300

    Scriptable Objects

---
+++ ZombieSurvivor/Assets/Script/Aula8/ScriptableObjects/WeaponScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (menuName ="WeaponScript")]
+public class WeaponScript : ScriptableObject {
+
+    public string weaponName;
+    public float radius = 5;
+    public float damage = 5;
+    public float ammo = 200;
+    public GameObject particle;
+}
